9fee1f85820ccb0ff912ad1a7543ffc2fc08098a
yadhukrishnankb-rgb <yadhukrishnan9188@gmail.com>
2026-04-27 13:03:29 +0530

    fix: google auth redirect issue

--- config/passport.js
+++ config/passport.js
@@ -3,34 +3,13 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/userSchema');
 
-// passport.use(new GoogleStrategy({
-//     clientID: process.env.GOOGLE_CLIENT_ID,
-//     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//     callbackURL: "/auth/google/callback"
-//   },
-//   async (accessToken, refreshToken, profile, done) => {
-//     try {
-//       // Find or create user
-//       let user = await User.findOne({ googleId: profile.id });
-//       if (!user) {
-//         user = await User.create({
-//           name: profile.displayName,
-//           email: profile.emails[0].value,
-//           googleId: profile.id
-//         });
-//       }
-//       return done(null, user);
-//     } catch (err) {
-//       return done(err, null);
-//     }
-//   }
-// ));
+
 
 passport.use(new GoogleStrategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: `${process.env.BASE_URL}/auth/google/callback`
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
